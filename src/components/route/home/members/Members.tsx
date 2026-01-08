302e1cb67eb18065226094b770d70b477a2955be
Sejar Parvez <sejarparvez@yahoo.com>
2026-01-08 13:58:35 +0600

    updated members section

--- src/components/route/home/members/Members.tsx
+++ src/components/route/home/members/Members.tsx
@@ -2,25 +2,23 @@ import React from "react";
 import { FaUsers } from "react-icons/fa";
 import img2 from "../../../../images/member/mahbub.jpg";
 import img1 from "../../../../images/member/mohon.jpg";
-import img from "../../../../images/member/shohidur.jpg";
 import SectionHeader from "../../../animation/SectionHeader";
 import MembersModel from "./MembersModel";
 
 export default function Members() {
-  return (
-    <div className="mx-2 flex flex-col items-center justify-center gap-10 text-center md:mx-20">
-      <SectionHeader
-        title="প্রতিষ্ঠানের সদস্যগণ"
-        text="আমাদের কোর্স সমূহ পরিচালনার জন্য রয়েছে একদল দক্ষ ইন্সট্রাক্টর যারা
+	return (
+		<div className="mx-2 flex flex-col items-center justify-center gap-10 text-center md:mx-20">
+			<SectionHeader
+				title="প্রতিষ্ঠানের সদস্যগণ"
+				text="আমাদের কোর্স সমূহ পরিচালনার জন্য রয়েছে একদল দক্ষ ইন্সট্রাক্টর যারা
           সরাসরি বিভিন্ন কাজের সাথে জড়িত"
-        icon={FaUsers}
-      />
+				icon={FaUsers}
+			/>
 
-      <div className="grid gap-10 md:grid-cols-3">
-        <MembersModel name="মোঃ শাহিদুর রহমান" title="পরিচালক" img={img} />
-        <MembersModel name="মোঃ মোহন" title="সিনিয়র ইন্সট্রাক্টর" img={img1} />
-        <MembersModel name="মোঃ মাহবুব" title="অফিস সহকারি" img={img2} />
-      </div>
-    </div>
-  );
+			<div className="grid gap-10 md:grid-cols-3">
+				<MembersModel name="মোঃ মোহন" title="পরিচালক" img={img1} />
+				<MembersModel name="মোঃ মাহবুব" title="অফিস সহকারি" img={img2} />
+			</div>
+		</div>
+	);
 }
